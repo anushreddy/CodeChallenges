ce9c931b11e225fb58f249f4fd621d9d205c19e0
Anush Reddy <anushreddy04@gmail.com>
2019-10-31 13:53:27 -0500

    created a new code challenge

---
+++ primes.rb
@@ -0,0 +1,36 @@
+def primes(n)
+  split_primes = Array.new
+  if n == 1 
+    puts 0
+  else
+    (2...n).each do |num|
+      split_primes << num if is_prime?(num)
+    end
+  end
+  # print split_primes
+  product_primes(split_primes, n)
+end
+
+def product_primes(arr, n)
+  max_array = []
+  for i in (0...arr.length) do
+    (0...arr.length).reverse_each do |j|
+      first = arr[i].to_i
+      last = arr[j].to_i
+
+      if (first + last == n)
+        max_array << (first * last)
+      end
+    end
+  end
+  puts max_array.max
+end
+
+def is_prime?(n)
+  (2...n).each do |d|
+    return false if n % d == 0
+  end 
+  true
+end
+
+puts primes(4) 
